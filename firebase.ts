88dde4da80bf0fcf253e779b18664c237c6e3b39
withdrw <finesse@usa.com>
2023-03-02 17:19:21 -0600

    added login page with firebase dynamically

---
+++ firebase.ts
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { getApps, initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBdkLh5wLY5Prn2glEYGVKsFKzESS_YbBE",
+  authDomain: "netflix-clone-d00ab.firebaseapp.com",
+//   databaseURL: "https://netflix-clone-d00ab.firebaseio.com",
+  projectId: "netflix-clone-d00ab",
+  storageBucket: "netflix-clone-d00ab.appspot.com",
+  messagingSenderId: "109199999999",
+  appId: "1:566610032348:web:3991e763fabefb0f28e619"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
+const db = getFirestore()
+const auth = getAuth()
+
+export default app
+export { auth, db }
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,8 +1,22 @@
+import Head from 'next/head'
 import React from 'react'
+import Image from 'next/image'
 
 function login() {
   return (
-    <div>Login</div>
+    <div className='relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent ' >
+            <Head>
+        <title>Netflix</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Image
+      alt=''
+        src="https://rb.gy/p2hphi"
+        layout="fill"
+        className="-z-10 !hidden opacity-60 sm:!inline"
+        objectFit="cover"
+      />
+    </div>
   )
 }
 
