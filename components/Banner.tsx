d338ecf4df77b6a3b6df2d02bed6af7576368034
withdrw <finesse@usa.com>
2023-03-01 22:57:59 -0600

    day 1 finished

---
+++ components/Banner.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react"
+import { baseUrl } from "../constants/movie"
+import { Movie } from "../typings"
+import Image from "next/image"
+import {FaPlay} from "react-icons/fa"
+import { InformationCircleIcon } from "@heroicons/react/outline"
+
+
+
+interface Props {
+  netflixOriginals: Movie[]
+
+
+}
+
+function Banner({ netflixOriginals }:Props) {
+  const [movie, setMovie] = useState<Movie | null>(null)
+
+  useEffect(() => {
+    setMovie(netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
+  )}, [netflixOriginals])
+  console.log(movie)
+
+  return (
+    <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12" >
+        <div className="absolute -z-10 top-0 left-0 h-[95vh] w-screen ">
+          <Image alt="" src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path }`} fill objectFit="cover" />
+        </div>
+    <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl">{movie?.title || movie?.name || movie?.original_name}</h1>
+    <p className="max-x-ws text-xs md:max-w-lg lg:max-w-2xl lg:text-2xl text-shadow-md" >{movie?.overview}</p>
+    
+    
+    <div className="flex space-x-3">
+    <button className="bannerButton bg-white text-black"><FaPlay className="h-4 w-4 text-black md:h-7 w-7"/>Play</button>
+    <button className="bannerButton bg-[gray]/70 "><InformationCircleIcon className="h-5 w-8 md:h-8 md:w-8"></InformationCircleIcon> More Info</button>
+
+    </div>
+
+    </div>
+  )
+}
+
+export default Banner
