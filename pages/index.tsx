0693be75371be8ff8c32dde6b4bcf2070722d548
withdrw <finesse@usa.com>
2023-03-29 15:31:08 -0500

    fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-fixed import Head from 'next/head'
+import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
 import requests from '../utils/request'
